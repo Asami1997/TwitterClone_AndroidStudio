ab1b0cff1096d02e6634c7afb1cb92b4dfc44ba9
Ahmad <asami1997@gmail.com>
2017-09-16 20:53:21 +0800

    Update StarterApplication.java

--- Twitter_Clone/src/main/java/com/parse/starter/StarterApplication.java
+++ Twitter_Clone/src/main/java/com/parse/starter/StarterApplication.java
@@ -23,30 +23,13 @@ public class StarterApplication extends Application {
 
     // Add your initialization code here
     Parse.initialize(new Parse.Configuration.Builder(getApplicationContext())
-            .applicationId("a93c1af009ef1e57119c2c868f45ebb70570816e")
-            .clientKey("0d17c7a0f5be6c17f62bb67759d9b2ebefcb38e0")
-            .server("http://ec2-18-220-64-12.us-east-2.compute.amazonaws.com:80/parse/")
+            .applicationId(" ** YOUR OWN ** ")
+            .clientKey(" ** YOUR OWN ** ")
+            .server("** YOUR OWN **")
             .build()
     );
 
-    /*
-    ParseObject object = new ParseObject("ExampleObject");
-    object.put("myNumber", "123");
-    object.put("myString", "rob");
-
-    object.saveInBackground(new SaveCallback () {
-      @Override
-      public void done(ParseException ex) {
-        if (ex == null) {
-          Log.i("Parse Result", "Successful!");
-        } else {
-          Log.i("Parse Result", "Failed" + ex.toString());
-        }
-      }
-    });
-
-*/
-   // ParseUser.enableAutomaticUser();
+ 
 
     ParseACL defaultACL = new ParseACL();
     defaultACL.setPublicReadAccess(true);
